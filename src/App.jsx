0b3a9f2c48f0a01523537c31b91d1ef82a1b46b2
FilipeAVNunes <filipenunes407@hotmail.com>
2020-03-04 23:09:08 +0000

    added new meal

--- src/App.jsx
+++ src/App.jsx
@@ -11,51 +11,88 @@ class App extends Component {
       meals: meals,
       name: '',
       calories: '',
-      image: ''
+      image: '',
+      active: false,
+      contentOfNewMeal: ''
     };
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.changeName = this.changeName.bind(this);
-  }
-
-  changeName(event) {
-    const value = event.target.value;
-
-    this.setState({
-      name: value
-    });
+    this.toggleForm = this.toggleForm.bind(this);
+    this.addMeal = this.addMeal.bind(this);
   }
 
   handleInputChange(event) {
+    event.preventDefault();
     const value = event.target.value;
-    const inputName = event.target.calories;
+    const inputName = event.target.name;
     this.setState({
       [inputName]: value
     });
   }
 
+  addMeal(event) {
+    event.preventDefault();
+    const data = {
+      name: this.state.name,
+      calories: this.state.calories,
+      image: this.state.image,
+      quantity: 0
+    };
+    const newMeals = [...this.state.meals, data];
+    this.setState({
+      meals: newMeals,
+      active: false,
+      contentOfNewMeal: ''
+    });
+  }
+
+  toggleForm() {
+    this.setState(previousState => ({
+      active: !previousState.active
+    }));
+  }
+
   render() {
-    const meals = this.state.meals;
+    //const meals = this.state.meals;
     return (
       <div className="App">
         <h1 className="title">Meals</h1>
-        <form className="form" onSubmit={this.sendMessage}>
-          <input
-            type="text"
-            placeholder="Name"
-            value={this.state.name}
-            onChange={this.changeName}
-          />
-          <input
-            type="text"
-            placeholder="Number of calories"
-            name="text"
-            onChange={this.handleInputChange}
-          />
-          <input type="file" value={this.state.image} />
-          <button>Add a meal</button>
-        </form>
+        <button onClick={this.toggleForm}>Add a meal?</button>
+        {this.state.active && (
+          <form className="form" onSubmit={this.addMeal} name="contentOfNewMeal">
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={this.state.name}
+              onChange={this.handleInputChange}
+            />
+            <input
+              type="number"
+              value={this.state.calories}
+              placeholder="Number of calories"
+              name="calories"
+              onChange={this.handleInputChange}
+            />
+            <input
+              type="text"
+              name="image"
+              value={this.state.image}
+              placeholder="Image Url"
+              onChange={this.handleInputChange}
+            />
+            <button>Add the meal to the list</button>
+          </form>
+        )}
         {this.state.meals.map(meal => {
-          return <MealBox name={meal.name} image={meal.image} calories={meal.calories}></MealBox>;
+          return (
+            <MealBox
+              name={meal.name}
+              key={meal.name}
+              image={meal.image}
+              calories={meal.calories}
+              quantity={meal.quantity}
+            ></MealBox>
+          );
         })}
       </div>
     );
